3c8c8635d38dd03272fedb9f77cb797af0a7679d
LainVellio <cvellio@gmail.com>
2020-10-17 18:46:27 +0300

    fix array in index.js and brain-even.js

--- games/brain-even.js
+++ games/brain-even.js
@@ -1,16 +1,19 @@
 import { randomInteger, isEven } from '../src/utils.js';
-import index from '../src/index.js';
+import { launchGame } from '../src/index.js';
 
 export default () => {
-  const arr = [];
-  arr[0] = 'Answer "yes" if the number is even, otherwise answer "no"';
-  for (let i = 1; i < 4; i += 1) {
-    arr[i] = randomInteger(1, 100);
-    if (isEven(arr[i]) === true) {
-      arr[i + 3] = 'yes';
-    } else {
-      arr[i + 3] = 'no';
-    }
+  const rules = 'Answer "yes" if the number is even, otherwise answer "no"';
+  const questions = [];
+  const answers = [];
+  const numQuestions = 10;
+
+  for (let i = 0; i < numQuestions; i += 1) {
+    const question = randomInteger(1, 100);
+    questions.push(question);
+    const answer = isEven(question) ? 'yes' : 'no';
+    answers.push(answer);
   }
-  index(arr);
+  // console.log('questions', questions);
+  // console.log('answers', answers);
+  launchGame(rules, questions, answers);
 };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import promptly from 'promptly';
 
-export default async (rightAnswer) => {
+export const launchGame = async (rules, questions, answers) => {
   console.log('Welcome to the Brain Games');
   const name = await promptly.prompt('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  console.log(rightAnswer[0]);
-  for (let i = 1; i < 4; i += 1) {
-    console.log(`Question: ${rightAnswer[i]}`);
+  console.log(rules);
+
+  for (let i = 0; i < questions.length; i += 1) {
+    const question = questions[i];
+    console.log(`Question: ${question}`);
     // eslint-disable-next-line no-await-in-loop
     const inputAnswer = await promptly.prompt('Your answer: ');
-    if (rightAnswer[i + 3] === inputAnswer) {
+    const answer = answers[i];
+
+    if (answer === inputAnswer) {
       console.log('Correct!');
     } else {
       console.log(
-        `'${inputAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer[i + 3]}'.\nLet's try again, ${name}!`,
+        `'${inputAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${name}!`,
       );
       return;
     }
   }
   console.log(`Congratulations, ${name}!`);
 };
+
+export default { launchGame };
